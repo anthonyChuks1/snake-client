db1bd00649a98cdf8f1ebce7f8b03b15771c67ff
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-11 00:32:14 -0400

    Added a name display on connect and a connect notification to the connect function

--- client.js
+++ client.js
@@ -2,14 +2,19 @@ const net = require("net");
 
 // estabishes a connection with the game server
 
-const connect  = function () {
+const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   conn.setEncoding('utf8');
 
-  conn.on('data',(data) =>{
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server.");
+    conn.write('Name: Roy')
+  })
+  
+  conn.on('data', (data) => {
     console.log(data);
   })
 
