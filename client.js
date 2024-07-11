c89c67bc6eab4eb7f3f2029ad6ab9b276841cb27
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-11 15:44:33 -0400

    Moved the handler to another file called input.js and exported it for use from the file, also added more comments

--- client.js
+++ client.js
@@ -1,21 +1,28 @@
 const net = require("net");
 
-// estabishes a connection with the game server
 
+
+/**
+ * Establishes a connection to the game server.
+ * @returns {net.Socket} The connection object.
+ */
 const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
+
+  //convert from hexadecimal to letters with ascii.
   conn.setEncoding('utf8');
 
+  //What to do after the connection protocol is complete.
   conn.on('connect', () => {
     console.log("Successfully connected to game server.");
     conn.write('Name: Roy')
     //conn.write('Move: up')
     
   })
-  
+  //Gets the data sent from the save; any... Data
   conn.on('data', (data) => {
     console.log(data);
   })
@@ -23,5 +30,7 @@ const connect = function () {
   return conn;
 };
 
-
-module.exports = connect;
+/*Exports here ⬇️*/
+module.exports = {
+  connect,
+};
--- play.js
+++ play.js
@@ -1,30 +1,10 @@
 
-const connect = require('./client')
-
-/**
- * Handles user input from stdin.
- * @param {string} key - The key pressed by the user.
- */
-const handleUserInput = function (key){
-  if(key === '\u0003'){
-    console.log(`Closing the connection...`);
-    process.exit();
-  }
-}
-
-/**
- * Sets up the interface to handle user input from stdin.
- * @returns {object} - The stdin object.
- */
-const setupInput = function () {
-  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on('data', handleUserInput);
-  return stdin;   // return the stdin object so we can use it elsewhere in the program
-};
+const {connect} = require('./client')
+const {setupInput} = require('./input')
+/************All imports above************************/
 
 setupInput();
+
 console.log('Connecting ...');
+
 connect();
